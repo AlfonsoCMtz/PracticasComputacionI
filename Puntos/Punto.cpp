6a38ef81717a0654f36bcfbd169f9a5ce8a50b95
AlfonsoCMtz <71724700+AlfonsoCMtz@users.noreply.github.com>
2020-11-04 12:05:53 -0600

    Punto1.1

--- Puntos/Punto.cpp
+++ Puntos/Punto.cpp
@@ -1,84 +1,51 @@
+//
+// Created by alfon on 04/11/2020.
+//
+
 #include <iostream>
 #include <array>
-
-// Incluimos nuestro encabezado con la definición de la clase Punto2D
+#include <cmath>
 #include "Punto.h"
+
+
 
 using namespace std;
 
-// Declaración de funciones
-void PedirValores(float& x, float& y);
-char PedirOperacion();
+void Punto2D::SetPosicion(float x, float y) {
 
-int main()
-{
-    // Creamos una nueva instancia de la clase Punt2D
-    Punto2D miPunto;
-    float x = 0, y = 0;
-    float temp1 = 0, temp2 = 0;
-    char operacion;
-    // Pedimos al usuario que ingrese las coordenadas iniciales del punto en el plano
-    cout << "Ingresa la posicion inicial del punto.\n";
-    PedirValores(x, y);
-    // Modificamos las coordenadas de la instancia
-    miPunto.SetPosicion(x, y);
-    // solicitamos al usuario que ingrese una operación válida a realizar
-    operacion = PedirOperacion();
-    // Similar a un if, pero con otra estructura.
-    switch (operacion)
-    {
-        // Si operacion == 't' entonces realiza lo siguiente
-        case 't':
-            // Solicitamos al usuario las coordenadas a desplazar el punto para cada eje
-            cout << "Ingresa los valores a desplazar para cada eje\n";
-            PedirValores(temp1, temp2);
-            // Trasladamos el objeto (modificamos sus coordenadas)
-            miPunto.Trasladar(temp1, temp2);
-            // Salimos del switch
-            break;
-            // Si operaciosn == 'r' entonces realiza lo siguiente
-        case 'r':
-            // Solicita al usuario ingrese el valor del ángulo a rotar
-            cout << "Ingresa el valor del angulo a rotar (en grados): ";
-            cin >> temp1;
-            // Rota el punto en el plano con respecto al origen
-            miPunto.RotarRespectoAlOrigen(temp1);
-            break;
-            // Si operacion == 'e' entonces realiza lo siguiente
-        case 'e':
-            // solicita al usuario que ingrese el factor de escala para cada eje
-            cout << "Ingresa los factores a escalar para cada eje\n";
-            PedirValores(temp1, temp2);
-            miPunto.Escalar(temp1, temp2);
-            break;
-            // Si ninguno de los casos anteriores se cumplió, entonces realiza lo siguiente
-        default:
-            cout << "Operacion no valida\n";
-            break;
+    XI=x;
+    YI=y;
+}
+
+void Punto2D::Escalar(float temp1, float temp2) {
+
+    XI = temp1 * XI;
+    YI = temp2 * YI;
+}
+
+void Punto2D::Trasladar(float temp1, float temp2) {
+
+    XI = temp1 + XI;
+    YI = temp2 + YI;
+}
+void Punto2D::RotarRespectoAlOrigen(float temp1) {
+
+        float r=sqrt((XI*YI)+(YI*YI));
+        float theta=atan(YI/XI);
+        temp1=(temp1*3.1415926535)/180;
+
+        XI=r*cos(theta+temp1);
+        YI=r*sin(theta+temp1);
+
+
+}
+
+float Punto2D::GetX() {
+    return YI;
+}
+
+float Punto2D::GetY() {
+        return XI;
     }
-    // Imprime la posición final del punto después de las transformaciones
-    cout << "Posicion final:\n";
-    cout << "[ " << miPunto.GetX() << " " << miPunto.GetY() << " ]";
-    // Si terminamos correctamente la ejecución del programa, regresamos 0
-    return 0;
-}
 
-// Solicita dos valores de punto flotante al usuario y modifícalos por referencia
-void PedirValores(float& x, float& y)
-{
-    cout << "x: ";
-    cin >> x;
-    cout << "y: ";
-    cin >> y;
-}
 
-// Solicita una operación válida al usuario
-char PedirOperacion()
-{
-    char op;
-    do {
-        cout << "Operacion ('t' para trasladar, 'r' para rotar, 'e' para escalar): ";
-        cin >> op;
-    } while (op != 't' && op != 'r' && op != 'e');
-    return op;
-}
--- Puntos/Punto.h
+++ Puntos/Punto.h
@@ -2,7 +2,29 @@
 // Created by alfon on 04/11/2020.
 //
 
-#ifndef PUNTO_PUNTO_H
-#define PUNTO_PUNTO_H
+#ifndef PUNTOS_PUNTO_H
+#define PUNTOS_PUNTO_H
 
-#endif //PUNTOS_PUNTO_H
+
+class Punto2D {
+
+public:
+    float XI{0}, YI{0};
+
+    void SetPosicion(float x, float y);
+
+    void Escalar(float temp1, float temp2);
+
+    void Trasladar(float temp1, float temp2);
+
+    void RotarRespectoAlOrigen(float temp1);
+
+
+    float GetX();
+
+    float GetY();
+
+
+};
+
+#endif //PUNTO_PUNTO_H
